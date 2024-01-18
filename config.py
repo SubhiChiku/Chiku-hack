110656d91d1d806c6e71c1c479d0a3b549366ebd
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-01-18 23:31:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
 class Config:
-    API_ID = "28483638"
-    API_HASH = "e890a4bc09691074f971235fca02706c"
-    TOKEN = "6878279819:AAFuq3bRkto5v1vKoeEAa-Ra6TYuJGWrNqw"
+    API_ID = "29626514"
+    API_HASH = "9460c8987ee9df06dd8fe22412d689ab"
+    TOKEN = "6840651563:AAFXRwpgl3gk65QhsQXF49QZ0cHtbdylJeg"
     MONGO_URL = "mongodb+srv://dostuahi:WeQKx8yemN268htW@cluster0.6fstll3.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/3464fb495af70bebb11fa.jpg"
-    SUDOERS = filters.user(["6847607464"])
+    SUDOERS = filters.user(["6322247029"])
